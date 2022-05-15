8676975d84b619901e4362cc0149eb19886cc4b6
Ilya Kolesnikov <zloy-gamer@yandex.ru>
2022-05-15 11:14:50 +0500

    Mod: api.urls, delete: admin, models, tests from api

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,25 +6,13 @@ from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 app_name = 'api_v1'
 
 router = routers.DefaultRouter()
-router.register(
-    r'posts',
-    PostViewSet,
-    basename='posts'
-)
-router.register(
-    r'groups',
-    GroupViewSet,
-    basename='groups'
-)
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
+router.register(r'follow', FollowViewSet, basename='follow')
 router.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
-)
-router.register(
-    r'follow',
-    FollowViewSet,
-    basename='follow'
 )
 
 urlpatterns = [
